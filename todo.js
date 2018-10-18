1d7336bea0754b014f6b3a21ccc9f8fc1b7dc093
EC2 Default User <ec2-user@ip-172-31-7-186.us-east-2.compute.internal>
2018-10-18 20:34:26 +0000

    files added

---
+++ todo.js
@@ -0,0 +1,48 @@
+/*global angular*/
+/*global todoDirective*/
+
+angular.module('app', [])
+  .controller('mainCtrl', mainCtrl)
+  .directive('tododir', todoDirective);
+
+function mainCtrl ($scope) {
+
+  $scope.todos = [];
+
+  $scope.addNew = function (todo) {
+    $scope.todos.push({ 
+      type: todo.type,
+      task: todo.task,
+      notes: todo.notes
+    }); 
+    
+    todo.type = ''; 
+    todo.task = '';
+    todo.notes ='';
+  };
+}
+
+function todoDirective () {
+  return {
+    scope: {
+      todo: '=' 
+    },
+    restrict: 'E', 
+    replace: 'true',
+    template: (
+      '<div class="todolist">' +
+        '<h2>{{todo.type}}</h2>' +
+        '<h5>{{todo.task}}</h5>' + 
+        '<h6>{{todo.notes}}</h6>' +
+      '</div>'
+    ), 
+   // link: link
+  };
+  
+  //function link (scope) { /* [4] */
+  //  if (!scope.user.avatarUrl) {
+  //    scope.user.avatarUrl = 'https://www.drupal.org/files/issues/default-avatar.png';
+  //  }
+ // }
+
+}
